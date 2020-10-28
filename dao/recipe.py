c00115c22df8f4bc69837d70c7740faa5ba5a684
Gordon Poon <31147056+rubinusp@users.noreply.github.com>
2020-10-28 11:10:56 -0600

    create the basic dao classes with sqlite3

---
+++ dao/recipe.py
@@ -0,0 +1,27 @@
+import sqlite3
+from sqlite3 import Error
+
+
+def create_connection(path):
+    connection = None
+    try:
+        connection = sqlite3.connect(path)
+        print("Connection to SQLite DB successful")
+    except Error as e:
+        print(f"The error '{e}' occurred")
+
+    return connection
+
+def create_table():
+
+    c = create_connection('yummy.db')
+
+    c.execute('')
+    c.commit()
+    c.close()
+
+def add_recipe():
+    pass
+
+def delete_recipe():
+    pass
---
+++ dao/user.py
@@ -0,0 +1,21 @@
+import sqlite3
+from sqlite3 import Error
+
+
+def create_connection(path):
+    connection = None
+    try:
+        connection = sqlite3.connect(path)
+        print("Connection to SQLite DB successful")
+    except Error as e:
+        print(f"The error '{e}' occurred")
+
+    return connection
+
+def create_table():
+
+    c = create_connection('yummy.db')
+
+    c.execute('')
+    c.commit()
+    c.close()
